f74e4f1690d4e85ab05c7ae5b35e19ffc0b94111
Pieter Sheth-Voss <pieter@protobi.com>
2015-03-14 00:28:00 -0400

    update bestbuy.R

--- bestbuy.R
+++ bestbuy.R
@@ -5,16 +5,33 @@ myProp <- read.table(secretLoc,header=FALSE, sep="=", row.names=1, strip.white=T
 BEST_BUY_API_KEY <- myProp["BEST_BUY_API_KEY",1]
 BEST_BUY_API_APPLICATION <- myProp["BEST_BUY_API_APPLICATION",1]
 
+## Query Best Buy API for products that match a specified query string
 query <- "iPhone"
-products_url <- paste(
+url_products <- paste(
   "http://api.remix.bestbuy.com/v1/products",
   "(longDescription=",query,"*)",
   "?show=","sku,name",
-  "&pageSize=","15",
-  "&page=","5",
+  "&pageSize=","10",
+  "&page=","1",
   "&format=","json",
   "&apiKey=",BEST_BUY_API_KEY,
+  sep="") 
+
+url_products
+
+products <- fromJSON(url_products)   
+#products$products$sku
+
+url_reviews <- paste(
+  "http://api.remix.bestbuy.com/v1/reviews(sku%20in(",
+  paste((products$products$sku), collapse=","),
+  "))",
+  "?format=json",
+  "&apiKey=",BEST_BUY_API_KEY,
+  "&show=id,sku,rating,comment,title",
+  "&pageSize=","100",
+  "&page=","1",
   sep="")
 
+reviews <- fromJSON(url_reviews)
 
-products <- fromJSON(products_url)
