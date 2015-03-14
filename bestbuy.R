0cca22f6f7cc474b73ce9ab7618b47118437fee2
Pieter Sheth-Voss <pieter@protobi.com>
2015-03-13 22:18:40 -0400

    update bestbuy.R

--- bestbuy.R
+++ bestbuy.R
@@ -4,5 +4,5 @@ myProp <- read.table("~/Exp/pogo/secret", header=FALSE, sep="=", row.names=1, st
 BEST_BUY_API_KEY <- myProp["BEST_BUY_API_KEY",1]
 BEST_BUY_API_APPLICATION <- myProp["BEST_BUY_API_APPLICATION",1]
 
-url1 <- paste("http://api.remix.bestbuy.com/v1/products(longDescription=iPhone*|sku=7619002)?show=sku,name&pageSize=15&page=5&format=json&apiKey=", BEST_BUY_API_KEY)
+url1 <- paste("http://api.remix.bestbuy.com/v1/products(longDescription=iPhone*|sku=7619002)?show=sku,name&pageSize=15&page=5&format=json&apiKey=", BEST_BUY_API_KEY,sep="")
 url1
