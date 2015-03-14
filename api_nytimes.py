a21463e2fe37aa47f393560127f9e2c72a01ad5b
Jinglin82@gmail.com <Jinglin82@gmail.com>
2015-03-14 14:02:42 -0400

    API NYT

--- api_nytimes.py
+++ api_nytimes.py
@@ -3,7 +3,6 @@ import requests
 import lxml.html as lh
 import re
 import sys 
-import csv
 
 def searchTopic(topic):
 	f = open('secret', 'r')
@@ -17,29 +16,30 @@ def searchTopic(topic):
 	headline = topic
 	sources = ['Reuters', 'AP', 'The New York Times']
 	beg_date = 20140101
-	pages = str(3)
+	pages = 2
 
 
-	articles = api.search(q = topic, fq = {'headline':topic, 'source':sources}, 
-		fl = ['web_url'], begin_date = beg_date, page = pages)
 	articles_content = []
 	output = []
-	for url in articles['response']['docs']:
-		if url['web_url'].find('video') == -1:
-			page = requests.get(url['web_url'])
-			doc = lh.fromstring(page.content)
-			text = doc.xpath('//p[@itemprop="articleBody"]')
-			article = str()
-			for par in text:
-				paragraph = par.text_content()
-				article += paragraph
-			articles_content.append(article)
-	for count, art in enumerate(articles_content):
-		regex = re.compile('[^a-zA-Z]')
-		articles_content[count] = regex.sub(' ', art)
-	with open('%s data.csv' % topic, 'wb') as output_file:
-		wr = csv.writer(output_file, quoting=csv.QUOTE_ALL)
-		wr.writerow(articles_content)
+	for page in range(pages):
+		articles = api.search(q = topic, fq = {'headline':topic, 'source':sources}, 
+					fl = ['web_url'], begin_date = beg_date, page = str(page + 1))
+		for url in articles['response']['docs']:
+			if url['web_url'].find('video') == -1:
+				page = requests.get(url['web_url'])
+				doc = lh.fromstring(page.content)
+				text = doc.xpath('//p[@itemprop="articleBody"]')
+				article = str()
+				for par in text:
+					paragraph = par.text_content()
+					article += paragraph
+				articles_content.append(article)
+		for count, art in enumerate(articles_content):
+			regex = re.compile('[^a-zA-Z]')
+			articles_content[count] = regex.sub(' ', art)
+	# with open('%s data.csv' % topic, 'wb') as output_file:
+	# 	wr = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+	# 	wr.writerow(articles_content)
 	print articles_content
 
 
@@ -51,5 +51,6 @@ if __name__ == '__main__':
 	str_input = ''
 	for x in range(len(sys.argv) - 1):
 		str_input = str_input + sys.argv[x + 1] + ' '
+	print str_input
 	searchTopic(sys.argv[1:])
 
