da26cd0f0283fd4ffd78b214bd70de5488343189
Ashwini Kumar <kumar_ashi@yahoo.com>
2015-03-14 13:16:20 -0400

    Added box plot with Sentiments

--- bestbuy.R
+++ bestbuy.R
@@ -1,8 +1,57 @@
+library(indicoio)
+library(jsonlite)
+
 
 myProp <- read.table(secretLoc,header=FALSE, sep="=", row.names=1, strip.white=TRUE, na.strings="NA", stringsAsFactors=FALSE)
 
 BEST_BUY_API_KEY <- myProp["BEST_BUY_API_KEY",1]
 BEST_BUY_API_APPLICATION <- myProp["BEST_BUY_API_APPLICATION",1]
 
-url1 <- paste("http://api.remix.bestbuy.com/v1/products(longDescription=iPhone*|sku=7619002)?show=sku,name&pageSize=15&page=5&format=json&apiKey=", BEST_BUY_API_KEY,sep="")
-url1
+#url1 <- paste(
+#  "http://api.remix.bestbuy.com/v1/products%28longDescription=iPhone*%29?show=sku,name&pageSize=100&page=5&format=json&apiKey=", BEST_BUY_API_KEY,sep="")
+
+query <- "iPhone"
+url_products <- paste(
+  "http://api.remix.bestbuy.com/v1/products",
+  "(longDescription=",query,"*)",
+  "?show=","sku,name",
+  "&pageSize=","10",
+  "&page=","1",
+  "&format=","json",
+  "&apiKey=",BEST_BUY_API_KEY,
+  sep="") 
+
+products <- jsonlite::fromJSON(url_products)   
+#products$products$sku
+
+url_reviews <- paste(
+  "http://api.remix.bestbuy.com/v1/reviews(sku%20in(",
+  paste((products$products$sku), collapse=","),
+  "))",
+  "?format=json",
+  "&apiKey=",BEST_BUY_API_KEY,
+  "&show=id,sku,rating,comment,title",
+  "&pageSize=","100",
+  "&page=","1",
+  sep="")
+
+reviews <- jsonlite::fromJSON(url_reviews)
+
+print (length(names(reviews)))
+
+nReviews <- nrow(reviews$reviews)
+reviews$reviews$sentiment <- NA
+#sentiments <- rep(NA, nReviews)
+for(i in 1:nReviews) {
+  reviews$reviews$sentiment[i] <- sentiment(reviews$reviews$comment[i])
+}
+
+#aggregate(reviews$reviews$sentiment, by=list(reviews$reviews$rating), FUN=mean)[2]
+boxplot(sentiment ~ rating, data= reviews$reviews, xlab = "rating", ylab = "sentiment")
+
+
+
+
+
+
+
