38830a1c75770ce49c1fb0f9efc07eec76c2b1ef
Pieter Sheth-Voss <pieter@protobi.com>
2015-03-13 23:58:29 -0400

    update bestbuy.R

--- bestbuy.R
+++ bestbuy.R
@@ -1,8 +1,20 @@
+library(jsonlite)
 
+## Read secret keys from a local file 
 myProp <- read.table(secretLoc,header=FALSE, sep="=", row.names=1, strip.white=TRUE, na.strings="NA", stringsAsFactors=FALSE)
-
 BEST_BUY_API_KEY <- myProp["BEST_BUY_API_KEY",1]
 BEST_BUY_API_APPLICATION <- myProp["BEST_BUY_API_APPLICATION",1]
 
-url1 <- paste("http://api.remix.bestbuy.com/v1/products(longDescription=iPhone*|sku=7619002)?show=sku,name&pageSize=15&page=5&format=json&apiKey=", BEST_BUY_API_KEY,sep="")
-url1
+query <- "iPhone"
+products_url <- paste(
+  "http://api.remix.bestbuy.com/v1/products",
+  "(longDescription=",query,"*)",
+  "?show=","sku,name",
+  "&pageSize=","15",
+  "&page=","5",
+  "&format=","json",
+  "&apiKey=",BEST_BUY_API_KEY,
+  sep="")
+
+
+products <- fromJSON(products_url)
